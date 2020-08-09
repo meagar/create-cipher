84a58230ac10dc3b092fbc0612510091fee97de5
Matthew Eagar <meagar@hey.com>
2020-08-09 10:10:27 -0400

    bundle gem create-cipher

---
+++ lib/create/cipher.rb
@@ -0,0 +1,8 @@
+require "create/cipher/version"
+
+module Create
+  module Cipher
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/create/cipher/version.rb
@@ -0,0 +1,5 @@
+module Create
+  module Cipher
+    VERSION = "0.0.1"
+  end
+end
